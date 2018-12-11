8174e2d1529784329cd70a28fb9160ea31228d0d
Aliaksandr Yerakhavets <Aliaksandr_Yerakhavets@epam.com>
2018-12-11 16:38:01 +0300

    webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
-module.exports = {
+module.exports = mode => ({
+  mode: mode,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'news.js'
+  },
   module: {
     rules: [
     { 
       test: /\.js$/,
       exclude: /node_modules/,
-      use: 'babel-loader' 
-    }
+      loader: 'babel-loader' 
+    },
+    { 
+      test: /\.css$/, 
+      loader: 'css-loader' 
+    },
   ]
-  }
-};
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'docs'),
+    compress: true,
+    port: 9000
+  },
+});
